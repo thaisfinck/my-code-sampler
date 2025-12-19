1fe8980d645b496b802592c2f99b3d51ffa28e29
thaisfinck <thaisfinck@gmail.com>
2025-12-19 11:24:44 -0300

    update the main page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,21 @@
+import { useState } from 'react';
+import Home from '../pages/Home';
+import Projects from '../pages/Projects';
+
 function App() {
-  return <>Hello, Visual Lab!</>;
+  const [activeTab, setActiveTab] = useState('home');
+  return (
+    <>
+      <header>
+        <button onClick={() => setActiveTab('home')}>Home</button>
+        <button onClick={() => setActiveTab('projects')}>Projects</button>
+      </header>
+      <main>
+        {activeTab === 'home' && <Home />}
+        {activeTab === 'projects' && <Projects />}
+      </main>
+    </>
+  );
 }
 
 export default App;
