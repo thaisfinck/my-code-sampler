639aaa4daa75b69334c1fc0be4a674ca1ad48dad
thaisfinck <thaisfinck@gmail.com>
2025-12-22 14:57:17 -0300

    add style to header

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,22 @@
 import { useState } from 'react';
 import Home from '../pages/Home';
 import Projects from '../pages/Projects';
+import '../styles/App.css';
+import { HomeIcon, ProjectIcon } from '../styles/icons';
 
 function App() {
   const [activeTab, setActiveTab] = useState('home');
   return (
     <>
       <header>
-        <button onClick={() => setActiveTab('home')}>Home</button>
-        <button onClick={() => setActiveTab('projects')}>Projects</button>
+        <button onClick={() => setActiveTab('home')}>
+          <HomeIcon />
+          HOME
+        </button>
+        <button onClick={() => setActiveTab('projects')}>
+          <ProjectIcon />
+          PROJECTS
+        </button>
       </header>
       <main>
         {activeTab === 'home' && <Home />}
