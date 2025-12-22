1cd94cb0cd265e5183a78394c52d4343c60db346
thaisfinck <thaisfinck@gmail.com>
2025-12-22 17:30:23 -0300

    add basename

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/App.tsx';
 
+const basename = import.meta.env.BASE_URL;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>
